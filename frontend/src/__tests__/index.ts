7efa0c66993e3aa24e7ab3faade88cf473e68445
Hernandez <lelehernandez2007@yahoo.fr>
2025-09-20 11:54:07 +0100

    fix: Supprimer physiquement les dossiers lourds node_modules et target pour réduire la taille du déploiement Vercel

--- frontend/src/__tests__/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CreationPage.test } from './CreationPage.test';
-export { default as MatchingPage.test } from './MatchingPage.test';
-export { default as MatchingResultsIA.test } from './MatchingResultsIA.test';
